d4ef77cccf6c6dc3f14911d6e15fcbdce718e06e
VitoBertucci <vito.rob.bertucci@gmail.com>
2021-10-04 21:15:12 -0400

    Started to make the second in class project with exceptions

--- BertucciAddSubtract.java/src/AddSubtract.java
+++ BertucciAddSubtract.java/src/AddSubtract.java
@@ -9,18 +9,20 @@ public class AddSubtract extends JFrame {
     //global widgets
     private TextField firstNumTField;
     private TextField secondNumTField;
-    private JButton addButton;
-    private JButton subtractButton;
+    private TextField thirdNumTField;
+
+    private JButton calculateButton;
+    private JButton exitButton;
 
     //global variables to use
     private int firstNum;
     private int secondNum;
-    private int sumDifference;
+    private double thirdNum;
     
     //main method
     public static void main(String[] args) {
         AddSubtract frame = new AddSubtract();
-        frame.setSize(300,200);
+        frame.setSize(500,200);
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
@@ -31,54 +33,57 @@ public class AddSubtract extends JFrame {
         Handler eventHandler = new Handler();
 
         //set title and layout
-        setTitle("Add and Subtract in Class Project");
+        setTitle("Triangle Calculator");
         c.setLayout(new FlowLayout());
 
         //add first label and textfield w/ event handler
-        Label firstNumLabel = new Label("Enter a number");
+        Label firstNumLabel = new Label("Side A");
         c.add(firstNumLabel);
         firstNumTField = new TextField(10);
         c.add(firstNumTField);
         firstNumTField.addActionListener(eventHandler);
 
         //add second label and textfield w/ event handler
-        Label secondNumLabel = new Label("Enter another number");
+        Label secondNumLabel = new Label("Side B");
         c.add(secondNumLabel);
         secondNumTField = new TextField(10);
         c.add(secondNumTField);
         secondNumTField.addActionListener(eventHandler);
 
+        Label thirdNumLabel = new Label("Side C");
+        c.add(thirdNumLabel);
+        thirdNumTField = new TextField(10);
+        c.add(thirdNumTField);
+        secondNumTField.addActionListener(eventHandler);
+
         //add add and subtract buttons w/ event handlers
-        addButton = new JButton("Add");
-        c.add(addButton);
-        addButton.addActionListener(eventHandler);
-        subtractButton = new JButton("Subtract");
-        c.add(subtractButton);
-        subtractButton.addActionListener(eventHandler);
+        calculateButton = new JButton("Calculate");
+        c.add(calculateButton);
+        calculateButton.addActionListener(eventHandler);
+        exitButton = new JButton("Exit");
+        c.add(exitButton);
+        exitButton.addActionListener(eventHandler);
     }
 
     //event handler for textfields
     private class Handler implements ActionListener {
-        public void actionPerformed(ActionEvent event) {
+        public void actionPerformed(ActionEvent event) throws NumberFormatException{
             //get text from textfields
-            firstNum = Integer.parseInt(firstNumTField.getText());
-            secondNum = Integer.parseInt(secondNumTField.getText());
-
-            //display user entries
-            System.out.println("The first number entered was: " + firstNum);
-            System.out.println("The second number entered was: " + secondNum);
-            System.out.println("Press either button to add or subtract the two numbers");
-
+                firstNum = Integer.parseInt(firstNumTField.getText());
+                secondNum = Integer.parseInt(secondNumTField.getText());
             //event handlers
-            if(event.getSource() == addButton) {
-                sumDifference = firstNum + secondNum;
-                System.out.println("The sum of the two numbers is: " + sumDifference);
-            } else {
-                sumDifference = firstNum - secondNum;
-                System.out.println("The difference of the two numbers is: " + sumDifference);
-            }
 
-            System.out.println("____________________________________________________________");
+            try {
+                if(event.getSource() == calculateButton) {
+                    thirdNum = Math.sqrt((firstNum * firstNum) + (secondNum * secondNum));
+                    System.out.println(thirdNum);
+                    thirdNumTField.setText(String.valueOf(thirdNum));
+                } else {
+                }
+            } catch (NumberFormatException nfe) {
+                System.out.println("Exception integer..." + nfe.toString()); 
+            }
+            
         }
     }
 }
